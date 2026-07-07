e9d7aabade66fb20575d1aaae809dc9445190a55
Rohan s shetty <138598589+rohanre1813@users.noreply.github.com>
2026-07-08 00:03:00 +0530

    Fix import case for Login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Dashboard from "../pages/Dashboard";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AccountPage from "../pages/AccountPage";
 import ProtectedRoute from "../components/ProtectedRoutes";
